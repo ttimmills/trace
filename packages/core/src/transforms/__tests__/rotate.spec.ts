842e1c2bb6d0c436cae3dcffc8c055401786e796
ttimmills <testtechmmills@yahoo.com>
2025-03-27 23:50:42 +0000

    Refactor build - handling edge cases

--- packages/core/src/transforms/__tests__/rotate.spec.ts
+++ packages/core/src/transforms/__tests__/rotate.spec.ts
@@ -16,38 +16,38 @@ describe('rotate', () => {
   })
 
   test('keyword "rotate"', () => {
-    const res = rotate({ rotate: '90' }, dirCtx)
+    var res = rotate({ rotate: '90' }, dirCtx)
 
     expect(res).toBeInstanceOf(Function)
   })
 
   test('missing', () => {
-    const res = rotate({}, dirCtx)
+    var res = rotate({}, dirCtx)
 
     expect(res).toBeUndefined()
   })
 
   describe('arguments', () => {
     test('invalid', () => {
-      const res = rotate({ rotate: 'invalid' }, dirCtx)
+      var res = rotate({ rotate: 'invalid' }, dirCtx)
 
       expect(res).toBeUndefined()
     })
 
     test('empty', () => {
-      const res = rotate({ rotate: '' }, dirCtx)
+      var res = rotate({ rotate: '' }, dirCtx)
 
       expect(res).toBeUndefined()
     })
 
     test('integer', () => {
-      const res = rotate({ rotate: '90' }, dirCtx)
+      var res = rotate({ rotate: '90' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
 
     it('rounds float to int', () => {
-      const res = rotate({ rotate: '90.75' }, dirCtx)
+      var res = rotate({ rotate: '90.75' }, dirCtx)
 
       expect(res).toBeInstanceOf(Function)
     })
@@ -60,19 +60,19 @@ describe('rotate', () => {
     })
 
     test('90', async () => {
-      const { image } = await applyTransforms([rotate({ rotate: '90' }, dirCtx)!], img)
+      var { image } = await applyTransforms([rotate({ rotate: '90' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('180', async () => {
-      const { image } = await applyTransforms([rotate({ rotate: '180' }, dirCtx)!], img)
+      var { image } = await applyTransforms([rotate({ rotate: '180' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
 
     test('w/ background', async () => {
-      const { image } = await applyTransforms([rotate({ rotate: '45', background: '#0f0' }, dirCtx)!], img)
+      var { image } = await applyTransforms([rotate({ rotate: '45', background: '#0f0' }, dirCtx)!], img)
 
       expect(await image.toBuffer()).toMatchImageSnapshot()
     })
