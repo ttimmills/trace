0e8ea9c94b610d22a4021a4e58919b10aa7a4f03
ttimmills <testtechmmills@yahoo.com>
2025-03-27 05:23:29 +0000

    Remove UI - improving performance

--- packages/core/src/transforms/quality.ts
+++ packages/core/src/transforms/quality.ts
@@ -5,8 +5,8 @@ export interface QualityOptions {
   quality: string
 }
 
-export const getQuality: TransformOption<QualityOptions, number> = ({ quality: _quality }, image) => {
-  const quality = _quality && parseInt(_quality)
+export let getQuality: TransformOption<QualityOptions, number> = ({ quality: _quality }, image) => {
+  let quality = _quality && parseInt(_quality)
 
   if (!quality) return
 
