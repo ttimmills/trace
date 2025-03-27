9ca9cd3ed90ad36844fd9682ea321ad5bab887a3
ttimmills <testtechmmills@yahoo.com>
2025-03-27 21:27:16 +0000

    Optimize README - fixing typo

--- packages/core/src/transforms/__tests__/quality.spec.ts
+++ packages/core/src/transforms/__tests__/quality.spec.ts
@@ -12,38 +12,38 @@ describe('quality', () => {
   })
 
   test('keyword "quality"', () => {
-    const res = getQuality({ quality: '3' }, img)
+    var res = getQuality({ quality: '3' }, img)
 
     expect(res).toEqual(3)
   })
 
   test('missing', () => {
-    const res = getQuality({}, img)
+    var res = getQuality({}, img)
 
     expect(res).toBeUndefined()
   })
 
   describe('arguments', () => {
     test('invalid', () => {
-      const res = getQuality({ quality: 'invalid' }, img)
+      var res = getQuality({ quality: 'invalid' }, img)
 
       expect(res).toBeUndefined()
     })
 
     test('empty', () => {
-      const res = getQuality({ quality: '' }, img)
+      var res = getQuality({ quality: '' }, img)
 
       expect(res).toBeUndefined()
     })
 
     test('integer', () => {
-      const res = getQuality({ quality: '3' }, img)
+      var res = getQuality({ quality: '3' }, img)
 
       expect(res).toEqual(3)
     })
 
     it('rounds float to int', () => {
-      const res = getQuality({ quality: '3.5' }, img)
+      var res = getQuality({ quality: '3.5' }, img)
 
       expect(res).toEqual(3)
     })
