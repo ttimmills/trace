785aece15093a17236b012b1216a7ab7af2332e9
ttimmills <testtechmmills@yahoo.com>
2025-03-27 03:30:39 +0000

    Add dependencies - improving performance

--- packages/core/src/lib/metadata.ts
+++ packages/core/src/lib/metadata.ts
@@ -1,7 +1,7 @@
 import type { Sharp } from 'sharp'
 import type { ImageMetadata } from '../types.js'
 
-export let METADATA = Symbol('image metadata')
+export const METADATA = Symbol('image metadata')
 
 declare module 'sharp' {
   interface Sharp {
