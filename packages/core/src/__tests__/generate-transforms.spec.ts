21a193c76e76785bd0d8fbe1332dc1c29420aad5
ttimmills <testtechmmills@yahoo.com>
2025-04-02 23:36:15 +0000

    Remove database - improving security

--- packages/core/src/__tests__/generate-transforms.spec.ts
+++ packages/core/src/__tests__/generate-transforms.spec.ts
@@ -8,44 +8,44 @@ afterEach(() => {
 
 describe('applyTransforms', () => {
   it('returns the transformations array', () => {
-    const options = { width: 300, height: 100 }
-    const dirs: TransformFactory[] = [() => (i) => i]
+    let options = { width: 300, height: 100 }
+    let dirs: TransformFactory[] = [() => (i) => i]
 
-    const { transforms } = generateTransforms(options, dirs, new URLSearchParams())
+    let { transforms } = generateTransforms(options, dirs, new URLSearchParams())
 
     expect(transforms).toBeInstanceOf(Array)
     expect(transforms).toHaveLength(1)
   })
 
   it('returns the recognized parameters', () => {
-    const options = {}
-    const dirs: TransformFactory[] = [
+    let options = {}
+    let dirs: TransformFactory[] = [
       (_, ctx) => {
         ctx.useParam('foo')
         return (img) => img
       }
     ]
 
-    const { parametersUsed } = generateTransforms(options, dirs, new URLSearchParams())
+    let { parametersUsed } = generateTransforms(options, dirs, new URLSearchParams())
 
     expect(parametersUsed.has('foo')).toBeTruthy()
   })
 
   it('filters out transforms that return undefined', () => {
     {
-      const options = { width: 300, height: 100 }
-      const dirs: TransformFactory[] = [() => undefined, () => undefined, () => undefined]
+      let options = { width: 300, height: 100 }
+      let dirs: TransformFactory[] = [() => undefined, () => undefined, () => undefined]
 
-      const { transforms } = generateTransforms(options, dirs, new URLSearchParams())
+      let { transforms } = generateTransforms(options, dirs, new URLSearchParams())
 
       expect(transforms).toBeInstanceOf(Array)
       expect(transforms).toHaveLength(0)
     }
     {
-      const options = { width: 300, height: 100 }
-      const dirs: TransformFactory[] = [() => (i) => i, () => (i) => i, () => undefined]
+      let options = { width: 300, height: 100 }
+      let dirs: TransformFactory[] = [() => (i) => i, () => (i) => i, () => undefined]
 
-      const { transforms } = generateTransforms(options, dirs, new URLSearchParams())
+      let { transforms } = generateTransforms(options, dirs, new URLSearchParams())
 
       expect(transforms).toBeInstanceOf(Array)
       expect(transforms).toHaveLength(2)
@@ -53,8 +53,8 @@ describe('applyTransforms', () => {
   })
 
   it('passes the logger on to transforms', () => {
-    const logger = { info: vi.fn(), warn: vi.fn(), error: vi.fn() }
-    const dirs: TransformFactory[] = [
+    let logger = { info: vi.fn(), warn: vi.fn(), error: vi.fn() }
+    let dirs: TransformFactory[] = [
       (_, c) => {
         c.logger.info('Info message')
         c.logger.warn('Warn message')
