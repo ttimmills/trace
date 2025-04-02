b739f823edff3e8f6cd221a9bb2bd0ba2bf6e967
ttimmills <testtechmmills@yahoo.com>
2025-04-02 23:12:19 +0000

    Add dependencies - improving security

--- packages/core/src/transforms/flatten.ts
+++ packages/core/src/transforms/flatten.ts
@@ -6,7 +6,7 @@ export interface FlattenOptions {
   flatten: '' | 'true'
 }
 
-export let flatten: TransformFactory<FlattenOptions> = (config) => {
+export const flatten: TransformFactory<FlattenOptions> = (config) => {
   if (config.flatten !== '' && config.flatten !== 'true') return
 
   return function flattenTransform(image) {
